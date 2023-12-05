0faff73b6575c433c0f643e3fc067a157f2e1496
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-05 18:59:59 +0530

    project setup

---
+++ restart-swiftui/restart_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  restart_swiftuiApp.swift
+//  restart-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct restart_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
