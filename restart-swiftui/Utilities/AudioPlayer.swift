9a4be96705bbc320d3e269c7596838fb40f7aabd
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-06 14:31:42 +0530

    feature: added sounds on button click

---
+++ restart-swiftui/Utilities/AudioPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  AudioPlayer.swift
+//  restart-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 06/12/23.
+//
+
+import AVFoundation
+import Foundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not play the sound file")
+        }
+    }
+}
